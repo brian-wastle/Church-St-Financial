33556f4495fa742963e383d700e8d06fc5bd4666
Brian Wastle <brian.wastle1@gmail.com>
2024-08-01 16:01:05 -0400

    authentication ui added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Amplify } from 'aws-amplify';
+import outputs from '../../amplify_outputs.json';
+import { AmplifyAuthenticatorModule, AuthenticatorService } from '@aws-amplify/ui-angular';
+
+
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AmplifyAuthenticatorModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Church-St-Financial';
+  constructor(public authenticator: AuthenticatorService) {
+
+  }
 }
